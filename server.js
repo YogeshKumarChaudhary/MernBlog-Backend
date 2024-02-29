542699692be9c497284288377be19dcc93fadec7
Chaudhary YogeshKumar <yogeshchaudhary9081@gmail.com>
2024-02-29 14:19:33 +0530

    origin added

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ require("./db");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 const corsOptions = {
   origin: "https://mernblogappbyyogesh.netlify.app",
-  methods: "GET, POST, PUT, DELETE,PATCH,HEAD",
+  methods: "GET,POST,PUT,DELETE,PATCH,",
   credentials: true,
 };
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
